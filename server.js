a3138732e53a7729952c901a66bc583d706013fd
rafael.miranda@mindera.com <rafael.miranda@mindera.com>
2020-11-15 01:33:29 +0000

    Add a lot of things

--- server.js
+++ server.js
@@ -1,14 +1,39 @@
 const express = require('express');
 const dotenv = require('dotenv');
+const morgan = require('morgan');
+const colors = require('colors');
+
+const connectDB = require('./config/db');
 
 // Load env vars
 dotenv.config({ path: './config/config.env' });
 
+// Connect to database
+connectDB();
+
+// Route files
+const bootcamps = require('./routes/bootcamps');
+
 const app = express();
+
+// Dev logging middleware
+if (process.env.NODE_ENV === 'development') {
+    app.use(morgan('dev'));
+}
+
+// Mount routers
+app.use('/api/v1/bootcamps', bootcamps);
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(
+const server = app.listen(
     PORT,
-    console.log(`Server running in ${process.env.NODE_ENV} mode on PORT ${PORT}`)
+    console.log(`Server running in ${process.env.NODE_ENV} mode on PORT ${PORT}`.yellow.bold)
 );
+
+// Handle unhandled promise rejection
+process.on('unhandledRejection', (err, promisse) => {
+    console.log(`Error: ${err.message}`.red);
+    // Close server & exit
+    server.close(() => process.exit(1));
+})
